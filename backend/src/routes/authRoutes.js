a1c008b41ae6255250f31c32ed6efb21b7ae2efd
Rahul Saini <rahulsaini2522@gmail.com>
2026-03-19 23:09:33 +0530

    file change

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,10 @@
-import express from 'express';
-import { register, login } from '../controllers/authController.js';
+import express from "express";
+import { register, login } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
+// Auth Routes
+router.post("/register", register);
+router.post("/login", login);
 
 export default router;
