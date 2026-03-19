7952d0bc31ed314cd1be06e53c74e2706e53bc8a
Rahul Saini <rahulsaini2522@gmail.com>
2026-03-19 23:09:55 +0530

    update

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,3 +1,8 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+// REGISTER
 export const register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -8,16 +13,23 @@ export const register = async (req, res) => {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    const user = await User.create({ name, email, password });
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword
+    });
 
     res.status(201).json({ message: "Registered", user });
 
   } catch (err) {
+    console.log(err);
     res.status(500).json({ message: "Error" });
   }
 };
 
-
+// LOGIN
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -41,6 +53,7 @@ export const login = async (req, res) => {
     res.json({ token });
 
   } catch (err) {
+    console.log(err);
     res.status(500).json({ message: "Error" });
   }
 };
